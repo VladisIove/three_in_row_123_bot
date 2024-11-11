9feacc5a48486ed5fb9fc0b2ed4bcdc0d3477ae4
Vlad Shelemakha <vlad.shelemakha0302@gmail.com>
2024-11-11 20:16:04 +0200

    add urls

--- user_core/urls.py
+++ user_core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user_core.views import MainPageWebApp
+from user_core.views import MainPageWebApp, GetOrCreateUser
 
 urlpatterns = [
-    path("", MainPageWebApp.as_view(template_name="about.html")),
+    path("", MainPageWebApp.as_view()),
+    path("/get_or_create_user", GetOrCreateUser.as_view()),
 ]
