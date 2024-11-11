49669a7b790a3a5b9b401be51d18557321e3d996
Vlad Shelemakha <vlad.shelemakha0302@gmail.com>
2024-11-11 20:09:43 +0200

    init user

--- user_core/urls.py
+++ user_core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from user_core.views import MainPageWebApp
 
 urlpatterns = [
-    path("/", MainPageWebApp.as_view(template_name="about.html")),
+    path("", MainPageWebApp.as_view(template_name="about.html")),
 ]
--- user_core/views.py
+++ user_core/views.py
@@ -1,5 +1,34 @@
 from django.views.generic import TemplateView
+from django.views import View
+from django import http
+from django.forms.models import model_to_dict
 
+from user_core.models import TelegramUser
+
+from typing import Any
 
 class MainPageWebApp(TemplateView):
     template_name = 'index.html'
+    
+
+class GetOrCreateUser(View):
+    
+    def get_or_create(self, tg_id, referral_token = None):
+        user = TelegramUser.objects.filter(telegram_id=tg_id).first()
+        if user: 
+            return model_to_dict(user)
+        referral = referral_token or TelegramUser.objects.filter(referral_token=referral_token).first()
+        
+        user = TelegramUser()
+        user.telegram_id = tg_id
+        user.referral = referral 
+        user.save()
+        return model_to_dict(user)
+        
+        
+    
+    def post(
+        self, request: http.HttpRequest, *args: Any, **kwargs: Any
+    ) -> http.HttpResponse:
+        
+        data = request.data 
