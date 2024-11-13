105c33f49e04b4fd80e5889035f2d310685e11de
Vlad Shelemakha <vlad.shelemakha0302@gmail.com>
2024-11-13 09:21:27 +0200

    hotfix

--- tg_app/settings.py
+++ tg_app/settings.py
@@ -132,7 +132,7 @@ TELEGRAM_BOT_TOKEN = "7573355657:AAEk6baXfcUfAFNgWKqMvqGwtnida9_eKkk"
 
 TELEGRAM_BOT = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-REFFERAL_LINK_TEXT = "https://t.me/three_in_row_123_bot?startapp={}"
+REFFERAL_LINK_TEXT = "https://t.me/three_in_row_123_bot?startApp={}"
 
 CORS_ALLOW_METHODS = (
     "DELETE",
