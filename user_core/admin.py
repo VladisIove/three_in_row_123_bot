8a70453c63f80294f1f1b8ce7944a3b7c7bbcd98
Vlad Shelemakha <vlad.shelemakha0302@gmail.com>
2025-01-15 12:30:40 +0200

    rollback

--- user_core/admin.py
+++ user_core/admin.py
@@ -7,8 +7,8 @@ class ChatInline(admin.TabularInline):
     model = AIChat
     extra = 0  
     can_delete = False
-    readonly_fields = ('rate', 'feedback', 'formatted_conversation')
-    fields = ('rate', 'feedback', 'formatted_conversation')
+    readonly_fields = ('rate', 'feedback', 'conversation')
+    fields = ('rate', 'feedback', 'conversation')
 
     def formatted_conversation(self, instance):
         if not instance.conversation:
